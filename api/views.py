51415f70badc5e1993a36baaf22890a4e7f28b67
jehoshaphat-obol <jeholo5464@gmail.com>
2025-03-28 22:37:32 +0300

    feature - create api views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+class StudentListView(generics.ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class SubjectListView(generics.ListAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
