7260b1fc90d6c03725ff5ce132f28a00f407feb4
jehoshaphat-obol <jeholo5464@gmail.com>
2025-04-18 21:42:06 +0300

    fix - updates.sh pulls from main branch

--- update.sh
+++ update.sh
@@ -21,7 +21,7 @@ fi
 # Pull the latest changes from the GitHub repository
 log_message "Pulling latest changes from GitHub repository..."
 cd /home/ubuntu/app || exit 1
-git pull origin master
+git pull origin main
 if [ $? -eq 0 ]; then
     log_message "Git pull successful."
 else
